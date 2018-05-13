0dba9dd00a3429916254067cbe72df8a36239122
zecojls <zecojls@gmail.com>
2018-05-13 19:59:59 -0300

    blog post

--- initial-setup.R
+++ initial-setup.R
@@ -1,7 +1,6 @@
-install.packages("blogdown")
+## install.packages("blogdown")
 library(blogdown)
-blogdown::install_hugo()
-new_site(theme = "nurlansu/hugo-sustain")
-blogdown::hugo_version()
-serve_site()
+## blogdown::install_hugo()
+## new_site(theme = "nurlansu/hugo-sustain")
+## blogdown::hugo_version()
 
